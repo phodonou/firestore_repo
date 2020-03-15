ba8edde772b13ccf275507ac1e02d09bf62dd044
Prince Hodonou <princehodonou@dhcp19.merunorth97.iit.edu>
2020-03-15 18:53:40 -0500

    added readme

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # firestore_repo
 
-A package that makes it easy to read and write firestore documents
+firestore_repo is a package that makes it easy to read and write Firestore Documents
+
+## Overview
+
+
+```Dart
+//Initialize repo
+FirestoreRepo db = FirestoreRepo();
+
+//Read one document
+db.getInstance(collectionPath: 'users', docId: 'enbjnsj1n3j39');
+
+//Read Multiple documents from a collection
+db.getInstanceAsLis5(collectionPath: 'users');
+
+//write one document
+db.setInstance(
+collectionPath: 'users', 
+docId: 'enbjnsj1n3j39', 
+data: {'name': 'Prince'},
+);
+
+//write one document, auto-generated id
+db.setInstance(
+collectionPath: 'users', 
+data: {'name': 'Prince'},
+);
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- lib/firestore_repo.dart
+++ lib/firestore_repo.dart
@@ -1,3 +1,71 @@
 library firestore_repo;
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:meta/meta.dart';
 
+class FirestoreRepo {
+  final Firestore _firestoreInstance = Firestore.instance;
+
+  Future<List<DocumentSnapshot>> getInstancesAsList({
+    @required String path,
+  }) async {
+    QuerySnapshot future =
+        await _firestoreInstance.collection(path).getDocuments();
+    return future.documents;
+  }
+
+  Future<DocumentSnapshot> getInstance({
+    @required String path,
+    @required String docId,
+  }) async {
+    DocumentSnapshot docSnap =
+        await _firestoreInstance.collection(path).document(docId).get();
+    return docSnap;
+  }
+
+  Future setInstance({
+    @required String path,
+    @required String docId,
+    @required Map<String, dynamic> data,
+  }) async {
+    DocumentReference ref = _firestoreInstance.collection(path).document(docId);
+    await ref.setData(data, merge: true).catchError((err) {
+      print(err);
+    });
+  }
+
+  Future<String> setInstanceNoID({
+    @required String path,
+    @required Map<String, dynamic> data,
+  }) async {
+    DocumentReference ref = _firestoreInstance.collection(path).document();
+    await ref.setData(data, merge: true);
+    return ref.documentID;
+  }
+
+  Future updateInstance({
+    @required String path,
+    @required String docId,
+    @required Map<String, dynamic> data,
+  }) async {
+    try {
+      await _firestoreInstance
+          .collection(path)
+          .document(docId)
+          .updateData(data);
+    } catch (e) {}
+  }
+
+  Future deleteInstance({
+    @required String path,
+    @required String docId,
+  }) async {
+    try {
+      await _firestoreInstance.collection(path).document(docId).delete();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Firestore get firestoreInstance => _firestoreInstance;
+}
